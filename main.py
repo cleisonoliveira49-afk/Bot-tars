4981322735757e1c6286d4ee0a5bc614615b1dcf
cleisonoliveira49-afk <cleisonoliveira49@gmail.com>
2025-08-06 13:31:25 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+import os
 
-BOT_TOKEN = "8254941787:AAFvYg8HK69leAkv5RmR_WockW2H1Ee8mWw"
+BOT_TOKEN = os.getenv("8254941787:AAFvYg8HK69leAkv5RmR_WockW2H1Ee8mWw")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Bot está vivo, meu chapa!")
+    await update.message.reply_text("Olá! Bot TARS online e funcional 😎")
 
-app = ApplicationBuilder().token(BOT_TOKEN).build()
-app.add_handler(CommandHandler("start", start))
-app.run_polling()
+if __name__ == "__main__":
+    app = ApplicationBuilder().token(BOT_TOKEN).build()
+    app.add_handler(CommandHandler("start", start))
+    app.run_polling()
+    
